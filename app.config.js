a37e4ba91f8f2bacd4966e2776aae5d1ec5bd99f
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-09-16 17:10:16 +0100

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -22,6 +22,8 @@ export default {
       "supportsTablet": true
     },
     android: {
+      "package": "com.todo.list",
+      "googleServicesFile": "./google-services.json",
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#FFFFFF"
