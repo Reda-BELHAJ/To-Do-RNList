a1bbffec88f0059a0e76a52225703cf08536e443
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-09-16 17:10:31 +0100

    Update SettingsScreen.js

--- src/Screens/SettingsScreen.js
+++ src/Screens/SettingsScreen.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import PaperButton from '../Components/PaperButton';
+import TextInput from '../Components/TextInput'
+import { TextInput as Input } from 'react-native-paper';
 import Firebase from '../../config/firebase';
 import { selectUser } from '../Reducer/authReducer'
 import { signOut } from '../Reducer/authReducer'
@@ -10,8 +12,16 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const auth = Firebase.auth();
 
 export default function SettingsScreen({ navigation }) {
+    const [ secure, setSecure ] = useState(true)
+    const [ info, setInfo ] = useState('')
     const user = useSelector(selectUser)
     const dispatch = useDispatch()
+
+    const getCred = async (token) => {
+        await Firebase.database().ref('users/' + token ).once("value").then(function(snapshot) {
+            setInfo(snapshot.val())
+        })
+    } 
     
     const handleSignOut = async () => {
         AsyncStorage.removeItem('token');
@@ -24,8 +34,38 @@ export default function SettingsScreen({ navigation }) {
         });
     };
 
+    useEffect(() => {
+        getCred(user.uid)
+    }, [])
+
     return (
         <View style={styles.container}>
+            <TextInput
+                disabled
+                label="UID"
+                returnKeyType="next"
+                value={user.uid}
+            />
+            <TextInput
+                disabled
+                label="Email"
+                returnKeyType="next"
+                value={info.email}
+            />
+            <TextInput
+                disabled
+                label="UserName"
+                returnKeyType="next"
+                value={info.name}
+            />
+            <TextInput
+                label="Password"
+                disabled
+                value={info.password}
+                secureTextEntry={secure}
+                right={<Input.Icon name="eye" onPress={() => {setSecure(!secure)}} />}
+            />
+
             <PaperButton mode="outlined" onPress={handleSignOut} >
                 Disconnect
             </PaperButton>
