7835a1948d318d6e4dbbfc97ad60b970621aa2c6
Reda BELHAJ <reda.belhaajj@gmail.com>
2021-09-16 17:10:18 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
+import Constants from 'expo-constants';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as Notifications from 'expo-notifications';
 import { store } from './Store';
 
 import HomeScreen from './src/Screens/HomeScreen';
@@ -56,7 +58,42 @@ const AuthScreens = () => (
   </AuthStack.Navigator>
 );
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+  }),
+});
+
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+      registerForPushNotificationsAsync().then(token =>
+          setExpoPushToken(token)
+      );
+
+      notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+          setNotification(notification);
+      });
+      responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      
+          const {notification: {request: {content: {data: {screen}}}}} = response
+          if (screen) {
+              props.navigation.navigate(screen)
+          }
+      });
+
+      return () => {
+          Notifications.removeNotificationSubscription(notificationListener.current);
+          Notifications.removeNotificationSubscription(responseListener.current);
+      };
+  }, []);
+
   return (
     <NavigationContainer>
       <ReduxProvider store={store}>
@@ -70,4 +107,37 @@ export default function App() {
       </ReduxProvider>
     </NavigationContainer>
   );
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+      const {status: existingStatus} = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+          const {status} = await Notifications.requestPermissionsAsync();
+          finalStatus = status;
+          console.log("existingStatus",existingStatus)
+      }
+      if (finalStatus !== 'granted') {
+          alert('Failed to get push token for push notification!');
+          console.log("finalStatus",finalStatus)
+          return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+  } else {
+      alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          showBadge: true,
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: '#FE9018',
+      });
+  }
+
+  return token;
 }
